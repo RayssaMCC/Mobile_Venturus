543e5ea94ce402cfafda32255514fe26b69b2723
Rayssa Mara Caldini Costa <rayssa_mcc@hotmail.com>
2025-08-11 11:15:02 -0300

    Aula 05/08

---
+++ receitas/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:receitas/di/service_locator.dart';
+import 'package:receitas/routes/app_router.dart';
+import 'package:receitas/utils/config/env.dart';
+import 'package:receitas/utils/theme/custom_theme_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  // Garante que o Flutter está inicializado
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Carregar variáveis de ambiente
+  await Env.init();
+
+  // Inicializando o Supabase
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
+
+  // Inicializando as dependências
+  await setupDependencies();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // * Get.put
+    // Usado para injetar dependências no GetX
+    final theme = Get.put(CustomThemeController());
+
+    // * Obx
+    // Usado para tornar um widget reativo
+    return Obx(
+      () => MaterialApp.router(
+        title: 'Eu Amo Cozinhar',
+        debugShowCheckedModeBanner: false,
+        theme: theme.customTheme,
+        darkTheme: theme.customThemeDark,
+        themeMode: theme.isDark.value ? ThemeMode.dark : ThemeMode.light,
+        routerConfig: AppRouter().router,
+      ),
+    );
+  }
+}
