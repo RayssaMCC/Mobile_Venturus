3c8b3086cdb319b707fb3e20f280eeaf33dfdc3f
Rayssa Mara Caldini Costa <rayssa_mcc@hotmail.com>
2025-08-30 12:07:45 -0300

    Aula 26/08

--- receitas/lib/ui/auth/auth_view.dart
+++ receitas/lib/ui/auth/auth_view.dart
@@ -16,26 +16,26 @@ class _AuthViewState extends State<AuthView>
   final viewModel = getIt<AuthViewModel>();
 
   late AnimationController _animationController;
-  late Animation<double> _animation;
+  // late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
 
-    _animationController =
-        AnimationController(
-          vsync: this,
-          duration: const Duration(milliseconds: 1000),
-        )..addStatusListener((listener) {
-          if (listener == AnimationStatus.completed) {
-            _animationController.reverse();
-          } else if (listener == AnimationStatus.dismissed) {
-            _animationController.forward();
-          }
-        });
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    // )..addStatusListener((listener) {
+    //   if (listener == AnimationStatus.completed) {
+    //     _animationController.reverse();
+    //   } else if (listener == AnimationStatus.dismissed) {
+    //     _animationController.forward();
+    //   }
+    // });
 
-    _animation = Tween(begin: 50.0, end: 200.0).animate(_animationController);
-    _animation.addListener(() => setState(() {}));
+    // _animation = Tween(begin: 50.0, end: 200.0).animate(_animationController);
+    // _animation.addListener(() => setState(() {}));
 
     _animationController.forward();
   }
@@ -118,10 +118,13 @@ class _AuthViewState extends State<AuthView>
           end: 200.0,
         ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
 
-        final colorTween = ColorTween(
-          begin: Theme.of(context).colorScheme.onError,
-          end: Theme.of(context).colorScheme.primary,
-        ).animate(CurvedAnimation(parent: controller, curve: Curves.bounceInOut));
+        final colorTween =
+            ColorTween(
+              begin: Theme.of(context).colorScheme.onError,
+              end: Theme.of(context).colorScheme.primary,
+            ).animate(
+              CurvedAnimation(parent: controller, curve: Curves.bounceInOut),
+            );
 
         final angleTween = Tween(
           begin: 0.0,
@@ -145,6 +148,7 @@ class _AuthViewState extends State<AuthView>
 
   Widget _buildEmailField() {
     return TextFormField(
+      key: ValueKey('emailField'),
       controller: viewModel.emailController,
       keyboardType: TextInputType.emailAddress,
       textInputAction: TextInputAction.next,
@@ -161,6 +165,7 @@ class _AuthViewState extends State<AuthView>
   Widget _buildPasswordField() {
     return Obx(
       () => TextFormField(
+        key: ValueKey('passwordField'),
         controller: viewModel.passwordController,
         obscureText: viewModel.obscurePassword,
         textInputAction: TextInputAction.done,
@@ -185,6 +190,7 @@ class _AuthViewState extends State<AuthView>
 
   Widget _buildConfirmPasswordField() {
     return TextFormField(
+      key: ValueKey('confirmPasswordField'),
       controller: viewModel.confirmPasswordController,
       obscureText: viewModel.obscurePassword,
       textInputAction: TextInputAction.done,
@@ -208,6 +214,7 @@ class _AuthViewState extends State<AuthView>
 
   Widget _buildUsernameField() {
     return TextFormField(
+      key: ValueKey('usernameField'),
       controller: viewModel.usernameController,
       textInputAction: TextInputAction.next,
       decoration: InputDecoration(
@@ -222,6 +229,7 @@ class _AuthViewState extends State<AuthView>
 
   Widget _buildAvatarUrlField() {
     return TextFormField(
+      key: ValueKey('avatarUrlField'),
       controller: viewModel.avatarUrlController,
       textInputAction: TextInputAction.done,
       decoration: InputDecoration(
@@ -254,6 +262,7 @@ class _AuthViewState extends State<AuthView>
     return SizedBox(
       height: 50,
       child: ElevatedButton(
+        key: ValueKey('submitButton'),
         onPressed: viewModel.submit,
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
@@ -290,6 +299,7 @@ class _AuthViewState extends State<AuthView>
           viewModel.isLoginMode ? 'Não tem uma conta? ' : 'Já tem uma conta? ',
         ),
         TextButton(
+          key: ValueKey('toggleButton'),
           onPressed: viewModel.isSubmitting ? null : viewModel.toggleMode,
           child: Text(
             viewModel.isLoginMode ? 'Cadastre-se' : 'Entre aqui',
